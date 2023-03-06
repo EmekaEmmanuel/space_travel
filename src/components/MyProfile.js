323dc73e757e0f4ab3a397d7156791b18814c6a9
jugosack <jugoslav_ackoski@yahoo.com>
2023-03-06 23:00:14 +0100

    Add cahnges in MyProfile.css, MyProfile.js and Nav.css

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import '../css/MyProfile.css';
 
 function MyProfile() {
   return (
-    <div className="app_container">MyProfile</div>
+    // <div className="app_container">MyProfile</div>
+    <section className="tableContainer">
+      <div className="missions">
+        <h1>My Missions</h1>
+        <div className="itemsList">
+          <div className="listBox">
+            <h2 className="listName">
+              Thaicom
+            </h2>
+          </div>
+          <div className="listBox">
+            <h2 className="listName">
+              SES
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="rockets">
+        <h1>My Rockets</h1>
+        <div className="itemsList">
+          <div className="listBox">
+            <h2 className="listName">
+              Falcon 1
+            </h2>
+          </div>
+          <div className="listBox">
+            <h2 className="listName">
+              Starship
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
